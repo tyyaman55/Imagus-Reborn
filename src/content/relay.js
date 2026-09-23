80309830e848763bfac5862beda6bf0cd833aeae
Talha Yasin <cobijaa@gmail.com>
2026-09-23 11:09:15 +0300

    Delete src/content directory

--- src/content/relay.js
+++
@@ -1,8 +0,0 @@
-// Firefox-only content script
-// Listens for messages from the background script and relays them to the user script context
-
-"use strict";
-
-chrome.runtime.onMessage.addListener(function(message) {
-    window.postMessage({ vdfDpshPtdhhd: "relay", message: message }, "*");
-});
